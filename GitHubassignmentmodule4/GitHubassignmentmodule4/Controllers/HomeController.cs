0b258cbfa47a27c12cbc95d473b09f6bd2786c28
Cristal Zavala <czavala7133@southwestuniversity.edu>
2021-03-11 20:42:04 -0700

    module 6

--- GitHubassignmentmodule4/GitHubassignmentmodule4/Controllers/HomeController.cs
+++ GitHubassignmentmodule4/GitHubassignmentmodule4/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace GitHubassignmentmodule4.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
